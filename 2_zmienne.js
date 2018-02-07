f7ad8ea87e69b463f3ca1316709704e820f03351
GodyckiCwirko <maxsimum200@wp.pl>
2018-02-07 13:29:47 +0100

    lekcja 3

---
+++ 2_zmienne.js
@@ -0,0 +1,43 @@
+var imie, nazwisko = "Godycki-Ćwirko";
+imie = "Janusz";
+document.write(imie);
+//+ konkatenacja
+document.write("<br>Masz na imię:" + imie + "<br>");
+nazwisko = "Godycki-Ćwirko";
+document.write("Twoje nazwisko to:" + nazwisko);
+
+
+var wiek = 16
+if (wiek <= 15)
+    document.write("<br>Jesteś dzieckiem");
+else if(wiek >= 65)
+    document.write("<br>Masz dużo lat");
+else if(wiek >=18)
+    document.write("<br>jesteś pełnoletni");
+else
+    document.write("<br>Nie jestes pełnoletni");
+
+
+
+
+//#####################################################
+
+var x, y;
+x = 10;
+y = 5;
+
+var suma = x + y;
+console.log("suma wynosi:"+ suma);
+var roznica = x - y;
+console.log(roznica);
+var dzielenie;
+dzielenie = x / y;
+mnozenie = x * y;
+modulo = x % y; // reszta z dzielenia
+console.log(modulo);
+
+
+
+
+
+
---
+++ typydanych.js
@@ -0,0 +1,7 @@
+var a = 10, b= "5a";
+var wynik = a + b;
+//rt(wynik);
+//t(a + b);
+console.log(typeof(a));
+
+
